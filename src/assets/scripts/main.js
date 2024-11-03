3272e22d0481d9ffdcdba56db2bb61cbdec6ce55
europokefreak <juan_velazquezg@hotmail.com>
2024-11-03 19:25:39 +0100

    Simplificacion del script de swiper

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -18,29 +18,10 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
-  
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-  },
-
-  
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
-
-  
-  autoplay: {
-    delay: 5,
-    disableOnInteraction: false,
-  },
+  }
 });
 
 +( function() {
